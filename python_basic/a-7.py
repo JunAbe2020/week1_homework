7323dfed0b85a2b4e8bfd4365e808b2ccbcc56b9
JunAbe2020 <aaabjny@gmail.com>
2025-09-22 04:14:16 +0900

    課題A: Python基本文法1 完了

---
+++ python_basic/a-7.py
@@ -0,0 +1,5 @@
+even_numbers = [2, 4, 6, 8]
+
+for numbers in even_numbers:
+    numbers *= 2
+    print(numbers)
---
+++ python_basic/a-8.py
@@ -0,0 +1,8 @@
+Users_info = [
+    ["Bob", 79],
+    ["Tom", 59],
+    ["Ken", 61],
+]
+
+for user in Users_info:
+    print(f"Name: {user[0]}, Age: {user[1]}")
